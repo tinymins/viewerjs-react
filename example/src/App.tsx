0c16d521f36906f006785da6f6e9e51e40487cc3
Emil Zhai <root@derzh.com>
2021-01-01 14:34:02 +0800

    feat: add viewer-js code logic

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'viewerjs-react'
+import RViewerJS from 'viewerjs-react'
 import 'viewerjs-react/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <RViewerJS>
+    <img
+      style={{ width: '40px', height: '40px', margin: '10px', border: '1px solid #cccccc', cursor: 'pointer' }}
+      src="https://www.google.com.hk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+      alt=""
+    />
+  </RViewerJS>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import ViewerJS from 'viewerjs'
+import 'viewerjs/dist/viewer.css'
 
-interface Props {
-  text: string
+interface RViewerJSProps {
+  className?: string
+  options?: ViewerJS.Options
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export default class RViewerJS extends React.PureComponent<RViewerJSProps> {
+  private viewer: ViewerJS
+
+  private onRef = (el: HTMLDivElement | null) => {
+    if (!el) {
+      return
+    }
+    if (this.viewer) {
+      this.viewer.destroy()
+    }
+    this.viewer = new ViewerJS(el)
+  }
+
+  public render() {
+    return (
+      <div ref={this.onRef} className={this.props.className}>
+        {this.props.children}
+      </div>
+    )
+  }
 }
