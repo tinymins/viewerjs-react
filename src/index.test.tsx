719eb5e0ce5917dc4739222f9bcef06e0af98a7e
Emil Zhai <root@derzh.com>
2021-01-04 13:59:27 +0800

    fix: test case

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import RViewerJS from '.'
 
-describe('ExampleComponent', () => {
+describe('RViewerJS', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(RViewerJS).toBeTruthy()
   })
 })
